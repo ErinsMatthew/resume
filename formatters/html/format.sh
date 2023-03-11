f3b70abd21e54d534849f6efaa1a385e7435ce52
Matthew W. Foster <github@matthewanderin.com>
2023-03-11 10:18:42 -0600

    Updated.

--- formatters/html/format.sh
+++ formatters/html/format.sh
@@ -4,7 +4,12 @@ set -o nounset
 
 JSON_TEMP_FILE=$(mktemp)
 
+# use yq to convert YAML to JSON
 yq . "$1" > "${JSON_TEMP_FILE}"
+
+OUTPUT_DIR=$(dirname "$2")
+
+cp formatters/html/html.css "${OUTPUT_DIR}"
 
 node formatters/html/html.js "${JSON_TEMP_FILE}" > "$2"
 
--- formatters/pdf/format.sh
+++ formatters/pdf/format.sh
@@ -3,11 +3,15 @@
 set -o nounset
 
 # format YAML as HTML
-HTML_TEMP_FILE=$(mktemp)
+TEMP_DIR=$(mktemp -d)
+
+cp formatters/html/html.css "${TEMP_DIR}"
+
+HTML_TEMP_FILE=${TEMP_DIR}/resume.html
 
 formatters/html/format.sh "$1" "${HTML_TEMP_FILE}"
 
 # convert temporary HTML file to PDF
-wkhtmltopdf - "$2" < "${HTML_TEMP_FILE}"
+wkhtmltopdf --enable-local-file-access "${HTML_TEMP_FILE}" "$2"
 
-rm "${HTML_TEMP_FILE}"
+rm -r "${TEMP_DIR}"
